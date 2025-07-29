607f30884324438da5814e7726ea394d2e10da9b
Hasmukh Patel <hasmukhlalpatel@live.co.uk>
2025-07-30 00:00:29 +0100

    Added generic schema registry service

---
+++ src/Kafka.Schemas.Shared/SchemaRegistryService.cs
@@ -0,0 +1,27 @@
+﻿using Confluent.SchemaRegistry;
+
+namespace Kafka.Schemas.Shared
+{
+    public class SchemaRegistryService : IDisposable
+    {
+        private readonly CachedSchemaRegistryClient _schemaRegistryClient;
+
+        public SchemaRegistryService()
+        {
+            var config = new SchemaRegistryConfig
+            {
+                Url = KafkaConfig.Default.SchemaRegistryUrl
+            };
+            _schemaRegistryClient = new CachedSchemaRegistryClient(config);
+        }
+        public SchemaRegistryService(SchemaRegistryConfig config)
+        {
+            _schemaRegistryClient = new CachedSchemaRegistryClient(config);
+        }
+
+        public void Dispose()
+        {
+            _schemaRegistryClient?.Dispose();
+        }
+    }
+}
