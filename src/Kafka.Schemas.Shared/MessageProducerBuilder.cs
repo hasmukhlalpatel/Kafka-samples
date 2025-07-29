2255bd7c8d0509c00b3f9fb35d57ef4ada30210b
Hasmukh Patel <hasmukhlalpatel@live.co.uk>
2025-07-30 00:00:56 +0100

    addded generic MessageProducerBuilder

---
+++ src/Kafka.Schemas.Shared/MessageProducerBuilder.cs
@@ -0,0 +1,71 @@
+﻿using Confluent.Kafka;
+using Confluent.SchemaRegistry;
+using Confluent.SchemaRegistry.Serdes;
+using System.Text;
+
+namespace Kafka.Schemas.Shared
+{
+    public class MessageProducerBuilder<TKey, TValue> :IDisposable
+        where TKey : class
+        where TValue : class
+    {
+        private readonly IProducer<TKey, TValue> _producer;
+
+        public MessageProducerBuilder()
+        {
+            var producerConfig = new ProducerConfig { BootstrapServers = KafkaConfig.Default.BootstrapServers };
+
+            var config = new SchemaRegistryConfig
+            {
+                Url = KafkaConfig.Default.SchemaRegistryUrl
+            };
+
+            var jsonSerializerConfig = new JsonSerializerConfig
+            {
+                AutoRegisterSchemas = false, // Set this back to true for auto-registration
+                UseLatestVersion = true,
+                LatestCompatibilityStrict = true,
+            };
+        }
+        public MessageProducerBuilder(ProducerConfig producerConfig,
+            SchemaRegistryConfig config, JsonSerializerConfig jsonSerializerConfig)
+        {
+            var schemaRegistry = new CachedSchemaRegistryClient(config);
+            _producer = new ProducerBuilder<TKey,TValue>(producerConfig)
+                        .SetValueSerializer(new JsonSerializer<TValue>(schemaRegistry, jsonSerializerConfig))
+                        .Build();
+        }
+
+        public async Task ProduceAsync(string topic, TKey key, TValue value, IReadOnlyDictionary<string,string> headers,
+            CancellationToken cancellationToken)
+        {
+            try
+            {
+                var messageHeaders = new Headers();
+                if (headers != null)
+                {
+                    foreach (var header in headers)
+                    {
+                        messageHeaders.Add(header.Key, Encoding.UTF8.GetBytes(header.Value));
+                    }
+                }
+                var message = new Message<TKey, TValue>
+                {
+                    Key = key,
+                    Value = value,
+                    Headers = messageHeaders
+                };
+                var deliveryResult = await _producer.ProduceAsync(topic, message, cancellationToken);
+                Console.WriteLine($"Message delivered to {deliveryResult.TopicPartitionOffset}");
+            }
+            catch (ProduceException<TKey, TValue> e)
+            {
+                Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+            }
+        }
+        public void Dispose()
+        {
+            _producer?.Dispose();
+        }
+    }
+}
