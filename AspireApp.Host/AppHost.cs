d690d0225b2d95cf85632216f6bf1d1b2ff1048a
Hasmukh Patel <hasmukhlalpatel@live.co.uk>
2026-03-07 18:50:38 +0000

    working all three togather- Kafka, zookeepr & Schema-registr

--- AspireApp.Host/AppHost.cs
+++ AspireApp.Host/AppHost.cs
@@ -1,6 +1,5 @@
 var builder = DistributedApplication.CreateBuilder(args);
-//var kafka = builder.AddKafka("kafka");
-// Create a custom network for Kafka infrastructure
+
 // Add Zookeeper container
 var zookeeper = builder.AddContainer("zookeeper", "confluentinc/cp-zookeeper")
     .WithImage("confluentinc/cp-zookeeper:7.6.0")
@@ -12,11 +11,11 @@ var zookeeper = builder.AddContainer("zookeeper", "confluentinc/cp-zookeeper")
 // Add Kafka container
 var kafka = builder.AddContainer("kafka", "confluentinc/cp-kafka")
     .WithImage("confluentinc/cp-kafka:7.6.0")
-    .WithEndpoint(port:9292, targetPort: 9092, name: "kafka-plaintext-host")
-    .WithEndpoint(port:29292, targetPort: 29092, name: "kafka-internal")
+    .WithEndpoint(port: 9092, targetPort: 9092, name: "kafka-plaintext-host")
+    .WithEndpoint(port: 29092, targetPort: 29092, name: "kafka-internal")
     .WithEnvironment("KAFKA_BROKER_ID", "1")
     .WithEnvironment("KAFKA_ZOOKEEPER_CONNECT", "zookeeper:2181")
-    .WithEnvironment("KAFKA_ADVERTISED_LISTENERS", "PLAINTEXT://localhost:29092,PLAINTEXT_HOST://localhost:9092")
+    .WithEnvironment("KAFKA_ADVERTISED_LISTENERS", "PLAINTEXT://kafka:29092,PLAINTEXT_HOST://localhost:9092")
     .WithEnvironment("KAFKA_LISTENER_SECURITY_PROTOCOL_MAP", "PLAINTEXT:PLAINTEXT,PLAINTEXT_HOST:PLAINTEXT")
     .WithEnvironment("KAFKA_INTER_BROKER_LISTENER_NAME", "PLAINTEXT")
     .WithEnvironment("KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR", "1")
@@ -27,10 +26,10 @@ var kafka = builder.AddContainer("kafka", "confluentinc/cp-kafka")
 // Add Schema Registry container
 var schemaRegistry = builder.AddContainer("schema-registry", "confluentinc/cp-schema-registry")
     .WithImage("confluentinc/cp-schema-registry:7.6.0")
-    .WithEndpoint(port:8081, targetPort: 8081, name: "schema-registry-port")
+    .WithEndpoint(port: 8081, targetPort: 8081, name: "schema-registry-port")
     .WithEnvironment("SCHEMA_REGISTRY_HOST_NAME", "schema-registry")
     .WithEnvironment("SCHEMA_REGISTRY_LISTENERS", "http://0.0.0.0:8081")
-    .WithEnvironment("SCHEMA_REGISTRY_KAFKASTORE_BOOTSTRAP_SERVERS", "localhost:9092")
+    .WithEnvironment("SCHEMA_REGISTRY_KAFKASTORE_BOOTSTRAP_SERVERS", "kafka:29092")
     .WithEnvironment("SCHEMA_REGISTRY_KAFKASTORE_SECURITY_PROTOCOL", "PLAINTEXT")
     .WithContainerNetworkAlias("schema-registry")
     .WaitFor(kafka);
